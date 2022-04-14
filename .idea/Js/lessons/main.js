bbc4df92c68c59da989bcc904d30856031515ae5
OlegMaidaniuk <omaydanuk@gmail.com>
2022-04-14 12:57:50 +0300

    final html

---
+++ .idea/Js/lessons/main.js
@@ -0,0 +1,16 @@
+// Ваше завдання, зробити так, щоб файл відкривався без помилок .
+
+let a = 100;
+let b = `500`;
+let c = "hello";
+let d = "okten";
+const x = 'constant value';
+var y = 143;
+a = 100;
+console.log(a);{
+let x ='new value';
+
+let b ='new value';
+}
+let result = a + y;
+// console(result);
