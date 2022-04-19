3a726e7f43a739344f3c96e0a04a48de1a2d4c49
OlegMaidaniuk <omaydanuk@gmail.com>
2022-04-19 22:03:56 +0300

    Jsy

---
+++ .idea/Js/lessons 2/js 2_.js
@@ -0,0 +1,107 @@
+// let x = + prompt(`give me X`);
+// if (x === 0) {
+//
+//     console.log(`Virno`)
+//
+// }
+// else {
+//
+//     console.log(`No Virno`)
+// }
+
+// let time = +prompt(`wth time is it ?`)
+// if (time > 16 && time < 17 ){
+//
+//     console.log(`evning`)
+//
+//
+// }else if(time > 18 && time <= 23) {
+//     console.log(`night`)
+//
+// }else if ( time > 0 && time <=14 ){
+//     console.log(`morning`)
+//
+//
+// }else if (time >14 && time <=15){
+//
+//     console.log(`lunch`)
+//
+// }else {
+//     console.log(`Ти шо Нарік ? Are you a drug addict?`)
+//
+//
+// }
+
+// let day = 20;
+// if (day >= 1 && day <= 11) {
+//     console.log(`decade 1`)
+//
+// } else if (day > 11 && day <= 18) {
+//
+//     console.log(`decade 2`)
+// }else if (day > 18 && day <=30){
+//     console.log(`decade 3`)
+//
+// }else{
+//     console.log(`wtf man ?!`)
+//
+// }
+
+// switch (+prompt("enter a day number")) {
+//     case 1:
+//         console.log(`I will write the first part of the site`)
+//         break;
+//     case 2:
+//         console.log(`I will write the second part of the site`)
+//         break;
+//     case 3:
+//         console.log(`I will write the third part of the site`)
+//         break;
+//     case 4:
+//         console.log(`I will write the fourth part of the site`)
+//         break;
+//     case 5:
+//         console.log(`I will write the final part of the site`)
+//         break;
+//     case 6:
+//         console.log(`I go to the pool`)
+//         break;
+//     case 7:
+//         console.log(`I will stay at home and watch movies`)
+//         break;
+//     default:
+// }
+
+// let a = 0;
+// let b = 0;
+//
+//
+// if (a < b) {
+//
+//     console.log(a)
+//
+// } else if (b > a) {
+//
+//     console.log(b)
+//
+// } else if (a === b){
+//
+//     console.log(`equal`)
+//
+// }
+// let a = 0;
+// let b = 0;
+// let c = 0;
+// if (a > b && a > c) {
+//     console.log(a)
+// } else if (b > a && b > c) {
+//     console.log(b)
+// } else if (c > a && c > b){
+//     console.log(c)
+// }else if (a===b&&b===c){
+//     console.log(`equal`);
+// }
+
+
+
+
